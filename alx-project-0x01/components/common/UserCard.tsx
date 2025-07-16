02aba19c40e6aae1d8f4a423155c2110af0888d5
Mokete <ematyabekana@gmail.com>
2025-07-16 22:32:09 +0200

    Updated the display grid of the users, fetched data from the public Api link,

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -1,25 +1,17 @@
 import { UserProps } from '@/interfaces';
 import React from 'react';
 
-const UserCard:React.FC<UserProps> = ({name, username, email, phone, website, address, company}) => {
+const UserCard:React.FC<UserProps> = ({name, username, email,  address}) => {
 
   return (
-    <div className="max-w-lg mx-auto p-6 bg-white shadow-lg rounded-lg">
+    <div className="max-w-xl  my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
       <h2 className="text-2xl font-semibold mb-2">{name} ({username})</h2>
       <p className="text-gray-700 mb-1">{email}</p>
-      <p className="text-gray-700 mb-1">{phone}</p>
-      <p className="text-gray-700 mb-1">{website}</p>
       <div className="mt-4">
         <h3 className="font-semibold">Address:</h3>
         <p>{address.street}, {address.suite}</p>
         <p>{address.city}, {address.zipcode}</p>
-        <p>📍 Lat: {address.geo.lat}, Lng: {address.geo.lng}</p>
-      </div>
-      <div className="mt-4">
-        <h3 className="font-semibold">Company:</h3>
-        <p>{company.name}</p>
-        <p><em>{company.catchPhrase}</em></p>
-        <p>{company.bs}</p>
+        <p> Lat: {address.geo.lat}, Lng: {address.geo.lng}</p>
       </div>
     </div>
   );
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -4,19 +4,21 @@ import UserCard from '../../components/common/UserCard';
 import Header from "@/components/layout/Header";
 import { UserProps } from "@/interfaces";
 
-const Users: React.FC = () => {
+const Users: React.FC<UserProps[]> = ({users}) => {
+     console.log(users)
   return (
       <div className="flex flex-col h-screen">
     <Header />
     <main className="py-4 px-10">
-      <div className="flex justify-between">
+      <div className="flex justify-between mb-4">
       <h1 className=" text-2xl font-semibold">Users Content</h1>
       <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
       </div>
-      <div className="grid grid-cols-3 gap-2 ">
+
+      <div className="grid grid-cols-3 gap-2 p-0 ">
         {
-          posts?.map(({ title, body, userId, id }: UserProps, key: number) => (
-            <PostCard title={title} body={body} userId={userId} id={id} key={key} />
+          users?.map(({  name, username, email,address }: UserProps, key: number) => (
+            <UserCard name={name} username={username} email={email} address={address} key={key} />
           ))
         }
       </div>
@@ -28,11 +30,11 @@ const Users: React.FC = () => {
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const posts = await response.json()
+  const users = await response.json()
 
   return {
     props: {
-      posts
+      users
     }
   }
 }
