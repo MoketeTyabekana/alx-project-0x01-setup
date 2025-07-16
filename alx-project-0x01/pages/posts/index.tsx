65845e1442547c030d3993d957532cae0f56ef55
Mokete <ematyabekana@gmail.com>
2025-07-16 20:13:52 +0200

    Created a react functional components for posts and users.

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Posts: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-7xl font-thin">Welcome To Posts Page</h1>
+    </div>
+  )
+}
+
+ export default Posts;
