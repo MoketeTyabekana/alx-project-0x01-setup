1c4d865632d3b9387e6c8fcee9a0698541be92b0
Mokete <ematyabekana@gmail.com>
2025-07-16 22:37:55 +0200

    renamed variable users to posts

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -4,20 +4,20 @@ import UserCard from '../../components/common/UserCard';
 import Header from "@/components/layout/Header";
 import { UserProps } from "@/interfaces";
 
-const Users: React.FC<UserProps[]> = ({users}) => {
-     console.log(users)
+const posts: React.FC<UserProps[]> = ({posts}) => {
+     console.log(posts)
   return (
       <div className="flex flex-col h-screen">
     <Header />
     <main className="py-4 px-10">
       <div className="flex justify-between mb-4">
-      <h1 className=" text-2xl font-semibold">Users Content</h1>
+      <h1 className=" text-2xl font-semibold">posts Content</h1>
       <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
       </div>
 
       <div className="grid grid-cols-3 gap-2 p-0 ">
         {
-          users?.map(({  name, username, email,address }: UserProps, key: number) => (
+          posts?.map(({  name, username, email,address }: UserProps, key: number) => (
             <UserCard name={name} username={username} email={email} address={address} key={key} />
           ))
         }
@@ -29,14 +29,14 @@ const Users: React.FC<UserProps[]> = ({users}) => {
 
 
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const users = await response.json()
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const posts = await response.json()
 
   return {
     props: {
-      users
+      posts
     }
   }
 }
 
- export default Users;
+ export default posts;
